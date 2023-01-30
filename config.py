07ddb0dcfa251c6cf554385fc7ea97cfef42413d
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-01-30 10:36:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5988361578:AAHukwb3OyI0BDGmkyVGszSbBSKjEnZUQbo")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5988361578:AAH52jSKPVuf1KWwo6y5oWBIzvEo8rI0YaY")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", "24281454"))
-    API_HASH = os.environ.get("API_HASH", "dbe4521b4291da85becb65c7d4d4c36c")
+    API_ID = int(os.environ.get("API_ID", "21818317"))
+    API_HASH = os.environ.get("API_HASH", "bc6ab154300cc41fe127ca4d658dc75d")
     AUTH_USERS = os.environ.get("OWNER", "5650200786")
